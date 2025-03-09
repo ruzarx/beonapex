3223930df038d88b44f774115026d82cae918eed
Igor Demidov <ruzarx@gmail.com>
2025-03-09 09:51:07 +0400

    Added overview and fixed group sorting

---
+++ src/components/OverviewTable.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Checkbox, FormControlLabel } from "@mui/material";
+import raceData from "../data/data.json"; 
+import { getAverageFinishPosition, getAverageStartPosition, getAverageFantasyPoints } from "../utils/raceUtils";
+
+const OverviewTable = ({ group, drivers, raceDates, track, useStar }) => {
+  const [excludePlayoffs, setExcludePlayoffs] = useState(false);
+
+  return (
+    <TableContainer component={Paper} sx={{ maxHeight: 700, overflow: "auto" }}>
+
+      <FormControlLabel
+        control={<Checkbox checked={excludePlayoffs} onChange={() => setExcludePlayoffs(!excludePlayoffs)} />}
+        label="Exclude Playoff Races"
+      />
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ width: "120px", fontWeight: "bold" }}>Driver</TableCell>
+            <TableCell sx={{ width: "80px", textAlign: "center" }}>Avg. Finish Pos</TableCell>
+            <TableCell sx={{ width: "80px", textAlign: "center" }}>Avg. Start Pos</TableCell>
+            <TableCell sx={{ width: "80px", textAlign: "center" }}>Avg. Fantasy Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {drivers.map((driver, index) => (
+            <TableRow key={index}>
+              <TableCell sx={{ width: "120px" }}>{driver}</TableCell>
+              <TableCell sx={{ width: "80px", textAlign: "center" }}>
+                {getAverageFinishPosition(raceData, driver, group, track, excludePlayoffs, useStar)}
+              </TableCell>
+              <TableCell sx={{ width: "80px", textAlign: "center" }}>
+                {getAverageStartPosition(raceData, driver, group, track, excludePlayoffs, useStar)}
+              </TableCell>
+              <TableCell sx={{ width: "80px", textAlign: "center" }}>
+                {getAverageFantasyPoints(raceData, driver, group, track, excludePlayoffs, useStar)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default OverviewTable;
